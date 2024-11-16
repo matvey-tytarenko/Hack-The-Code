aee1edbb3dd98a0f1c2d5470ead877b1123705cf
“Matvey <matveytytarenko25@gmail.com>
2024-11-16 21:10:42 +0100

    front

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -4,9 +4,26 @@ import './App.css'
 function App() {
   return (
     <>
-    <h1>Hello World!</h1>
+      <div className="container">
+        <h1 className="text-5xl font-bold top-0 left-0 p-10">Hack The Code</h1>
+        <form>
+          <div className="name">
+            <input
+              type="text"
+              placeholder="Input your username"
+              className="text-center"
+              style={{
+                "font-size": "25px",
+              }}
+            />
+          </div>
+          <div className="submit p-11">
+            <button type="submit" className='text-3xl hover: via-violet-700'>Play</button>
+          </div>
+        </form>
+      </div>
     </>
-  )
+  );
 }
 
 export default App
